2b9892c7ac7ab60b59153e67fbc00fc8ff5bcf6f
Karan Dubey <136021382+karandubey006@users.noreply.github.com>
2025-04-25 18:51:58 -0500

    please...

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//     plugins: [react()],
+//     base: '/landing-site/',
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     base: '/landing-site/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true,
+    }
 })
