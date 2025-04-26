7363bb70c9232b70c9429bb19de4c7aa1eaf5f45
Karan Dubey <136021382+karandubey006@users.noreply.github.com>
2025-04-25 19:18:24 -0500

    push

--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
     base: '/landing-site/',
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-        emptyOutDir: true,
-    }
+    plugins: [react()],
 })
